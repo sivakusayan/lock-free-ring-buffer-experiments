40fb5a81309e51ddae4702a05bf8f9ee836b0135
Sayan Sivakumaran <sivakusayan@gmail.com>
2025-03-08 10:43:54 -0800

    Make ring-buffer-size tests pass

--- ring-buffer.c
+++ ring-buffer.c
@@ -1,4 +1,4 @@
-#include "ring-buffer.h" 
+#include "ring-buffer.h"
 
 struct ring_buffer_header {
     size_t count;
@@ -9,9 +9,12 @@ struct ring_buffer_header {
 
 struct ring_buffer {
     struct ring_buffer_header header;
-    char data[];
+    char buffer[];
 };
 
 size_t ring_buffer_size(size_t count, size_t item_size) {
-    return sizeof(struct ring_buffer_header) + count * item_size;
+    if (count > SIZE_MAX / item_size) return 0;
+    size_t buffer_size = count * item_size;
+    if (buffer_size > SIZE_MAX - sizeof(struct ring_buffer_header)) return 0;
+    return sizeof(struct ring_buffer_header) + buffer_size;
 };
