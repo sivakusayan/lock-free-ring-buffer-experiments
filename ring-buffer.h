8c26707ad9e7d9840ced0b285e1c55baff855dc5
Sayan Sivakumaran <sivakusayan@gmail.com>
2025-03-08 08:58:24 -0800

    Flesh out interface to return better error codes

--- ring-buffer.h
+++ ring-buffer.h
@@ -7,25 +7,36 @@
 #define RING_BUFFER_OK 0
 #define RING_BUFFER_EMPTY -1
 #define RING_BUFFER_FULL -2
+#define RING_BUFFER_INVALID_ARGS -3
 
 /**
  * Returns the size, in bytes, of the desired ring buffer.
- * Returns 0 if the computed size overflows. 
+ * Returns 0 if the computed size overflows.
  */
 size_t ring_buffer_size(size_t count, size_t item_size);
 /**
- * Initializes a ring buffer into the provided buffer. The caller should use
- * ring_buffer_size() to determine how big the buffer should be.
+ * Returns RING_BUFFER_INVALID_ARGS if ring_buffer is null, or if the
+ * computed size overflows. Else returns RING_BUFFER_OK, and initializes
+ * a ring buffer into the provided memory.
+ *
+ * The caller should use ring_buffer_size() to determine how large the
+ * allocated memory being pointed to should be.
  */
-void ring_buffer_init(void* ring_buffer, size_t count, size_t item_size);
+int ring_buffer_init(void* ring_buffer, size_t count, size_t item_size);
 /**
- * Returns RING_BUFFER_FULL if the ring buffer has no more room, else
- * returns RING_BUFFER_OK.
- */
+ * Returns RING_BUFFER_INVALID_ARGS if either the ring_buffer or item
+ * are null pointers.
+ * Returns RING_BUFFER_FULL if the ring buffer has no more room.
+ * Else, returns RING_BUFFER_OK and appends a copy of the data pointed
+ * to by item to the start of the ring buffer.
+ **/
 int ring_buffer_push(void* ring_buffer, void* item);
 /**
- * Returns RING_BUFFER_EMPTY if the ring buffer has no items to pop,
- * else returns RING_BUFFER_OK.
+ * Returns RING_BUFFER_INVALID_ARGS if either the ring buffer or item
+ * are null pointers.
+ * Returns RING_BUFFER_EMPTY if the ring buffer has no items to pop.
+ * Else, returns RING_BUFFER_OK and copies the member at the end of
+ * the ring buffer into the item pointer, then removes that member.
  */
 int ring_buffer_pop(void* ring_buffer, void* item);
 
